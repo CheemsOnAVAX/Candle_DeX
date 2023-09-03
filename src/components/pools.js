a09fd9ad53b8cd925f2758be978f830ab27add46
Md Istiak Hussain <info.mdistiakhussain@gmail.com>
2023-09-03 10:40:53 +0600

    Update pools.js

--- src/components/pools.js
+++ src/components/pools.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { ERC20ABI,  pairABI} from "../contracts/ERC20";
+import { ERC20ABI, pairABI, nativeTicker, USDC, USDT, WNATIVE} from "../contracts/ERC20";
+import Token from  "../components/tokenChoice"
 import { Input,  Popover, Radio, Modal, message} from "antd";
 import {TabList, Tab, Skeleton} from "@web3uikit/core";
 import connection from "ethereactjs/menu";
@@ -8,7 +9,7 @@ import PoolCard from "./poolCard";
 
 
 export default function Pools(props){
-    const {address,signer, factory, router} = props;
+    const {address,signer, factory, router, provider, receive} = props;
     const [tokenOneSymbol, setSymbolOne] = useState("assetOne");
     const [tokenTwoSymbol, setSymbolTwo] = useState("assetTwo");
     const [tokenOne, setTokenOne] = useState(null);
@@ -16,6 +17,8 @@ export default function Pools(props){
     const [pair, setPair] = useState(null);
     const [changeToken , setChangeToken] = useState(0);
     const [isOpen, setIsOpen] = useState(false);
+    const [token , setToken] = useState(false);
+    const [input, setInput] = useState('')
     console.log(factory)
 
     function openModal(asset) {
@@ -24,17 +27,42 @@ export default function Pools(props){
     }
 
     const getContract = (e)=>{
-        
           connection(ERC20ABI, e.target.value).then(async(res)=>{
           console.log(changeToken)
           console.log(res)
           changeToken == 1 ? setTokenOne(res) : setTokenTwo(res)
           changeToken == 1 ? setSymbolOne(await res.symbol()) : setSymbolTwo(await res.symbol());
+          setToken(res.address);
         }).catch((err)=>console.log(err))
       }
 
+    function listedToken(token){
+      if(token == "native"){
+        changeToken == 1? setSymbolOne(nativeTicker) : setSymbolTwo(nativeTicker)
+        changeToken == 1? setTokenOne({address: WNATIVE}) : setTokenTwo({address: WNATIVE})
+      }else{
+        connection(ERC20ABI, token).then(async(res)=>{
+          console.log(changeToken)
+          console.log(res)
+          changeToken == 1 ? setTokenOne(res) : setTokenTwo(res)
+          changeToken == 1 ? setSymbolOne(await res.symbol()) : setSymbolTwo(await res.symbol());
+        }).catch((err)=>console.log(err))       
+      }
+      setIsOpen(false);
+      setToken(false)
+      setInput("Enter token address....")
+
+    }
+
+    async function modifyToken(){
+      setIsOpen(false);
+      setInput('')
+      setToken(false)
+    }
+
       useEffect(()=>{
-        async function getPair(){if(tokenOne != null && tokenTwo != null){
+
+        async function getPair(){
             const pairAddress = await factory.getPair(tokenOne.address, tokenTwo.address);
             connection(ERC20ABI, pairAddress).then((res)=>{
                 console.log(res)
@@ -42,8 +70,11 @@ export default function Pools(props){
             }).catch((err)=>{
                 console.log(err)
             })
-        }}
-        getPair()
+        }
+        console.log(tokenOneSymbol)
+        console.log(tokenTwoSymbol)
+        if(tokenOne != null && tokenTwo != null){
+        getPair()}
       }, [tokenOne, tokenTwo])
 
     return(
@@ -57,20 +88,18 @@ export default function Pools(props){
       >
         <div className="modalContent">
              
-        <Input className="tokenSearchBox"
-                  placeholder="enter valid token address" 
-                  disabled= {signer == null ? true : false}
-                  onChange={getContract}
-                  />
-             
-              <div
-                className="tokenChoice"
-                onClick={()=>setIsOpen(false)}
-              >
-                <div className="tokenChoiceNames">
-                  <div className="tokenName">{changeToken === 1 ? tokenOneSymbol : tokenTwoSymbol}</div>
-                </div>
-              </div>
+        <input className="searchInput" placeholder="Enter token address..."  value={input} onClick={()=>setInput("")} onChange={getContract} onFocus={()=>setInput("")} />
+        {token == false ? 
+        <div className="tokenList">
+         <Token listed={()=>listedToken("native")} tokenName={nativeTicker} provider={provider} token={null} address= {address} />
+         <Token listed={()=>listedToken(USDC)} tokenName={'stAVES'} token= {USDC}  address= {address} />
+         {/* <Token listed={()=>listedToken(USDT)} tokenName={'USDT'} token= {USDT}  address= {address} /> */}
+         <Token listed={()=>listedToken(WNATIVE)} tokenName={`W${nativeTicker}`} token= {WNATIVE}  address= {address} />
+
+         </div>
+          : <div className="tokenList">
+          <Token modify={modifyToken} tokenName={changeToken == 1 ? tokenOneSymbol : tokenTwoSymbol} provider={null} token={token} address={address} />
+         </div> }
            
         </div>
       </Modal>
@@ -94,8 +123,8 @@ export default function Pools(props){
             <div style={{ width: '100%', height: '100%' }}>
                 <div > Input tokens pair to get your liquidity position</div>
             </div>
-        </div> : pair == null && tokenOne != null && tokenTwo != null ?
-        <div
+        </div> : tokenOne != null && tokenTwo != null ?  <PoolCard pair={pair} address={address} signer={signer} tokenOne={tokenOne.address} tokenTwo={tokenTwo.address} tickerOne={tokenOneSymbol} tickerTwo={tokenTwoSymbol} router={router} receive={receive}/>
+       : <div
         style={{
             display: 'flex',
             padding: '7px',
@@ -110,7 +139,7 @@ export default function Pools(props){
         <div style={{ width: '100%', height: '100%' }}>
             <Skeleton theme="text" />
         </div> </div> 
-        :  <PoolCard pair={pair} address={address} signer={signer} tokenOne={tokenOne.address} tokenTwo={tokenTwo.address} tickerOne={tokenOneSymbol} tickerTwo={tokenOneSymbol} router={router}/>}
+        }
          </div>
          
         </>
